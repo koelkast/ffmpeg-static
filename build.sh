9de1e113010544fccb64c373a6c16e1eafdad389
Jean Jacques Warmerdam <sjaq@koelka.st>
2012-01-13 20:06:41 +0100

    Update libpng, add libfaac and use latest ffmpeg

--- build.sh
+++ build.sh
@@ -11,23 +11,23 @@ rm -rf "$BUILD_DIR" "$TARGET_DIR"
 mkdir -p "$BUILD_DIR" "$TARGET_DIR"
 
 # NOTE: this is a fetchurl parameter, nothing to do with the current script
-#export TARGET_DIR_DIR="$BUILD_DIR"
+export TARGET_DIR_DIR="$BUILD_DIR"
 
 echo "#### FFmpeg static build, by STVS SA ####"
 cd $BUILD_DIR
-../fetchurl "http://www.tortall.net/projects/yasm/releases/yasm-1.1.0.tar.gz"
-../fetchurl "http://zlib.net/zlib-1.2.5.tar.bz2"
-../fetchurl "http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz"
-../fetchurl "http://downloads.sourceforge.net/project/libpng/libpng15/1.5.5/libpng-1.5.5.tar.gz"
-../fetchurl "http://downloads.xiph.org/releases/ogg/libogg-1.2.0.tar.gz"
-../fetchurl "http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.1.tar.bz2"
-../fetchurl "http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2"
-../fetchurl "http://webm.googlecode.com/files/libvpx-v0.9.7-p1.tar.bz2"
-../fetchurl "http://downloads.sourceforge.net/faac/faac-1.28.tar.bz2"
-../fetchurl "http://inde.koelka.st/x264.tar.gz"
-../fetchurl "http://downloads.xvid.org/downloads/xvidcore-1.3.2.tar.gz"
-../fetchurl "http://downloads.sourceforge.net/project/lame/lame/3.98.4/lame-3.98.4.tar.gz?use_mirror=auto"
-../fetchurl "http://ffmpeg.org/releases/ffmpeg-0.8.4.tar.gz"
+./fetchurl "http://www.tortall.net/projects/yasm/releases/yasm-1.1.0.tar.gz"
+./fetchurl "http://zlib.net/zlib-1.2.5.tar.bz2"
+./fetchurl "http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz"
+./fetchurl "http://heanet.dl.sourceforge.net/project/libpng/libpng15/1.5.7/libpng-1.5.7.tar.gz"
+./fetchurl "http://downloads.xiph.org/releases/ogg/libogg-1.2.0.tar.gz"
+./fetchurl "http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.1.tar.bz2"
+./fetchurl "http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2"
+./fetchurl "http://webm.googlecode.com/files/libvpx-v0.9.7-p1.tar.bz2"
+./fetchurl "http://downloads.sourceforge.net/faac/faac-1.28.tar.bz2"
+./fetchurl "http://ftp.videolan.org/pub/x264/snapshots/last_x264.tar.bz2"
+./fetchurl "http://downloads.xvid.org/downloads/xvidcore-1.3.2.tar.gz"
+./fetchurl "http://downloads.sourceforge.net/project/lame/lame/3.98.4/lame-3.98.4.tar.gz?use_mirror=auto"
+./fetchurl "http://heanet.dl.sourceforge.net/project/faac/faac-src/faac-1.28/faac-1.28.tar.gz"
 
 echo "*** Building yasm ***"
 cd "$BUILD_DIR/yasm-1.1.0"
@@ -45,7 +45,7 @@ make
 make install PREFIX=$TARGET_DIR
 
 echo "*** Building libpng ***"
-cd "$BUILD_DIR/libpng-1.5.5"
+cd "$BUILD_DIR/libpng-1.5.7"
 ./configure --prefix=$TARGET_DIR --enable-static --disable-shared
 make -j 4 && make install
 
@@ -77,9 +77,9 @@ cd "$BUILD_DIR/faac-1.28"
 # FIXME: gcc incompatibility, does not work with log()
 sed -i -e "s|^char \*strcasestr.*|//\0|" common/mp4v2/mpeg4ip.h
 make -j 4 && make install
-
+# 
 echo "*** Building x264 ***"
-cd "$BUILD_DIR/x264"
+cd $BUILD_DIR/x264-snapshot-*
 ./configure --prefix=$TARGET_DIR --enable-static --disable-shared
 make -j 4 && make install
 
@@ -95,13 +95,20 @@ cd "$BUILD_DIR/lame-3.98.4"
 ./configure --prefix=$TARGET_DIR --enable-static --disable-shared
 make -j 4 && make install
 
+echo "*** Building faac ***"
+cd "$BUILD_DIR/faac-1.28"
+./configure --prefix=$TARGET_DIR --enable-static --disable-shared
+make -j 4 && make install
+
+
 # FIXME: only OS-sepcific
 rm -f "$TARGET_DIR/lib/*.dylib"
 rm -f "$TARGET_DIR/lib/*.so"
-
-# FFMpeg
+# 
+# # FFMpeg
+echo "*** Fetching FFmpeg ***"
+git clone https://github.com/FFmpeg/FFmpeg.git "$BUILD_DIR/ffmpeg-latest"
 echo "*** Building FFmpeg ***"
-cd "$BUILD_DIR/ffmpeg-0.8.4"
-./configure --prefix=${OUTPUT_DIR:-$TARGET_DIR} --extra-version=static --disable-debug --disable-shared --enable-static --extra-cflags=--static --disable-ffplay --disable-ffserver --disable-doc --enable-gpl --enable-pthreads --enable-postproc --enable-gray --enable-runtime-cpudetect --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-bzlib --enable-zlib --enable-nonfree --enable-version3 --enable-libvpx --disable-devices
+cd "$BUILD_DIR/ffmpeg-latest"
+./configure --prefix=${OUTPUT_DIR:-$TARGET_DIR} --extra-version=static --disable-debug --enable-libvpx --enable-libx264 --disable-shared --disable-ffplay --disable-ffserver --disable-doc --enable-gpl --enable-pthreads --enable-postproc --enable-gray --enable-runtime-cpudetect --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-bzlib --enable-zlib --enable-nonfree --enable-version3 --enable-libvpx --disable-devices --enable-decoder=prores --enable-libfaac
 make -j 4 && make install
-
